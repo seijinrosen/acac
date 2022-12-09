7610068ef9a2c2b8c7ce96c6dfdbc3a38edfac41
seijinrosen <86702775+seijinrosen@users.noreply.github.com>
2022-12-09 23:02:12 +0900

    acac init コマンドを実装

--- acac/config.py
+++ acac/config.py
@@ -44,13 +44,19 @@ class Config(BaseModel):
     language: Language
 
 
-def load_config() -> Config:
+def init() -> None:
+    if ACAC_TOML.exists():
+        console.print("acac.toml がこのディレクトリ内に既に存在します。")
+        return
+    shutil.copy(DEFAULT_ACAC_TOML, ACAC_TOML)
+    console.print("[bold]Created:", ACAC_TOML)
+
+
+def load() -> Config:
     if not ACAC_TOML.exists():
-        console.print("このディレクトリ内に acac.toml が見つかりませんでした。")
+        console.print("acac.toml がこのディレクトリ内に見つかりませんでした。")
         if confirm_yN("acac.toml を作成しますか？"):
-            shutil.copy(DEFAULT_ACAC_TOML, ACAC_TOML)
-            console.print("Created:", ACAC_TOML, "\n")
+            init()
         else:
             sys.exit("Bye.")
-
     return Config(**tomli.loads(ACAC_TOML.read_text(encoding=UTF_8)))
